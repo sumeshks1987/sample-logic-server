cc01b0598fc15013c754cb1dbce36d11e779c815
System Administrator <root@Sumeshs-MacBook-Pro.local>
2017-05-10 16:12:54 +0530

    updated runLogic.js

--- src/runLogic.js
+++ src/runLogic.js
@@ -4,8 +4,25 @@ module.exports = function runLogic(eventData) {
   return new Promise((resolve) => {
     const client = InitClient.create(eventData, {succeed: resolve})
 
+    const provideOptions = client.createStep({
+    	satisfied(){ return false },
+
+    	promt() {
+    		client.addTextResponse('Hi, how can help you with IndusInd Bank related queries. Here are some quick links')
+    		client.addTextResponse('IFSC Code,Branches,Personal Banking,Corporate Banking,NRI Banking,Customer Support')
+    		client.done()
+    	}
+    })
+
+    client.runFlow({
+    	streams: {
+    		main: 'greeting',
+    		greeting: [provideOptions],
+    	}
+    })
+
     // Add your custom logic here!
-    client.addTextResponse('Responding from `runLogic.js`!')
-    client.done()
+    //client.addTextResponse('Responding from `runLogic.js`!')
+    //client.done()
   })
 }
