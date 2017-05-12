7757602e9e7263ac732158aa152da7f059b74a41
System Administrator <root@Sumeshs-MacBook-Pro.local>
2017-05-12 11:13:47 +0530

    runLogic.js has been updated

--- src/runLogic.js
+++ src/runLogic.js
@@ -91,7 +91,7 @@ module.exports = function runLogic(eventData) {
     	prompt() {
     		console.log("Test7");
     		const conversation = client.getConversation()
-    		console.log(client.getMessagePart('content'))
+    		console.log(client.getMessagePart()['content'])
     		if(client.getConversationState().type == "deposit"){
     			client.addTextResponse('Visit this URL for more details - http://www.indusind.com/')
     		} else {
